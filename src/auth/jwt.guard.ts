3d96933359371f37733fe4cbd70b48d22a9290cb
Nguyen BuI <longro3000@gmail.com>
2020-08-27 22:16:51 +0300

    add jwt guard

--- src/auth/jwt.guard.ts
+++ src/auth/jwt.guard.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
-import * as jwt from 'jsonwebtoken'
+import {
+  Injectable,
+  ExecutionContext,
+  CanActivate,
+  UnauthorizedException,
+} from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+import { Reflector } from '@nestjs/core'
 
-import {}
+import { JWT_AUTH_USER, NO_AUTH } from './auth.const'
+import { UserType } from '../auth/auth.type'
+
+@Injectable()
+export class UserJwtAuthGuard extends AuthGuard(JWT_AUTH_USER) {
+  constructor(private reflector: Reflector) {
+    super()
+  }
+
+  handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
+    const noAuth = this.reflector.get<string[]>(NO_AUTH, context.getHandler())
+    if (noAuth) {
+      return user
+    }
+    return super.handleRequest(err, user, info, context)
+  }
+}
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(
+    private userGuard: UserJwtAuthGuard,
+    private reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    let userAuth
+
+    // Handler context takes priority
+    let userTypes =
+      this.reflector.get<any>('USER_TYPES', context.getHandler()) ||
+      this.reflector.get<any>('USER_TYPES', context.getClass())
+
+    if (!(typeof userTypes === 'object')) {
+      userTypes = [userTypes]
+    }
+
+    try {
+      userAuth = await this.userGuard.canActivate(context)
+    } catch (_e) {}
+
+    if (userTypes.includes(UserType.User) && Boolean(userAuth)) {
+      return true
+    }
+
+    throw new UnauthorizedException()
+  }
+}
