6416d7b76fdbc3ede083f5ad48fd8db777601e96
Nguyen BuI <longro3000@gmail.com>
2020-09-17 13:02:03 +0300

    install passport local

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,4 +7,20 @@ import { User } from './user.entity'
 import { AppCrudController } from '../app.decorator'
 import { AppFeature } from '../app.type'
 import AuthService from '../auth/auth.service'
-import 
+import BypassAuth from '../auth/bypass-auth.decorator'
+
+@AppCrudController(AppFeature.Users, {
+  model: {
+    type: User
+  },
+  query: {
+
+  }
+})
+export class UserController implements CrudController<User> {
+  constructor(public service: UserService, private authService: AuthService) {}
+
+  @Post('login')
+  @BypassAuth()
+  @UseGuards()
+}
