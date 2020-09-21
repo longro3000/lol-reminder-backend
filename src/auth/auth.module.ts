59bb8906963849e051446acb30f675045a3225a5
Nguyen BuI <longro3000@gmail.com>
2020-09-21 14:02:41 +0300

    config auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,54 +1,39 @@
+import { AppAuthGuard } from './auth.guard';
+
 import { Module, Global } from '@nestjs/common'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
 
-import {
-  UserGoogleIdAuthStrategy,
-  StudentGoogleIdAuthStrategy,
-} from './google-id.strategy'
-import { UserJwtAuthStrategy, StudentJwtAuthStrategy } from './jwt.strategy'
+import { UserJwtAuthStrategy } from './jwt.strategy'
 import AuthService from './auth.service'
 import {
-  StudentJwtAuthGuard,
   UserJwtAuthGuard,
   JwtAuthGuard,
 } from './jwt.guard'
-import {
-  StudentGoogleIdAuthGuard,
-  UserGoogleIdAuthGuard,
-} from './google-id.guard'
-import { UsersModule } from '../users/users.module'
-import { StudentsModule } from '../students/students.module'
+import { AuthStrategy } from './auth.strategy'
+import { UserModule } from '../user/user.module'
 import { AppConfigModule } from '../config/config.module'
 import { CrudPermissionGuard } from './permission.guard'
 
 @Global()
 @Module({
   imports: [
-    StudentsModule,
-    UsersModule,
+    UserModule,
     PassportModule,
     JwtModule.register({}),
     AppConfigModule,
   ],
   providers: [
     AuthService,
-    UserGoogleIdAuthStrategy,
-    StudentGoogleIdAuthStrategy,
+    AuthStrategy,
     UserJwtAuthStrategy,
-    StudentJwtAuthStrategy,
-    StudentJwtAuthGuard,
     UserJwtAuthGuard,
-    StudentGoogleIdAuthGuard,
-    UserGoogleIdAuthGuard,
     JwtAuthGuard,
     CrudPermissionGuard,
   ],
   exports: [
-    StudentJwtAuthGuard,
-    StudentGoogleIdAuthGuard,
     UserJwtAuthGuard,
-    UserGoogleIdAuthGuard,
+    AppAuthGuard,
     JwtAuthGuard,
     AuthService,
     CrudPermissionGuard,
