4bc7b5c2d718e7eaef044094add31bf23c37b215
Nguyen BuI <longro3000@gmail.com>
2020-09-22 12:09:43 +0300

    add match module

--- src/match/match.module.ts
+++ src/match/match.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
+import { NotePackModule } from '../note-pack/note-pack.module'
 import { MatchService } from './match.service'
 import { Match } from './match.entity'
+import { MatchController } from './match.controller'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Match])],
+  imports: [
+    TypeOrmModule.forFeature([Match]),
+    forwardRef(() => NotePackModule)
+  ],
   providers: [MatchService],
+  controllers: [MatchController],
+  exports: [MatchService]
 })
 export class MatchModule {}
