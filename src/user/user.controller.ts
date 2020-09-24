78c4c8275095f51c7ffd9c69c752c1df9f092ed7
Nguyen BuI <longro3000@gmail.com>
2020-09-24 13:16:18 +0300

    add check usuername and email availability controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { CrudController } from '@nestjsx/crud'
-import { Post, Req, UseGuards } from '@nestjs/common'
+import { Post, Req, UseGuards, Body, BadRequestException } from '@nestjs/common'
 import { Request } from 'express'
 
 import { UserService } from './user.service'
@@ -21,10 +21,40 @@ import { AppAuthGuard } from '../auth/auth.guard'
 export class UserController implements CrudController<User> {
   constructor(public service: UserService, private authService: AuthService) {}
 
+  get base(): CrudController<User> {
+    return this
+  }
+
   @Post('login')
   @BypassAuth()
   @UseGuards(AppAuthGuard)
   login(@Req() req: Request) {
     return this.authService.login(req.user)
+  }
+
+  @Post('check-email')
+  @BypassAuth()
+  async checkMail(@Body('email') email: string) {
+    const existedEmail = await this.service.findOne({
+      where: {
+        email: email
+      }
+    })
+
+    if (existedEmail) throw new BadRequestException('Given email is not available')
+    return 'Given email is available'
+  }
+
+  @Post('check-username')
+  @BypassAuth()
+  async checkUsername(@Body('username') username: string) {
+    const existedUsername = await this.service.findOne({
+      where: {
+        username: username
+      }
+    })
+
+    if (existedUsername) throw new BadRequestException('Given username is not available')
+    return 'Given email is available'
   }
 }
