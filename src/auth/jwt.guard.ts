952a7327e71792e40225003784fbe85a01027d5f
Nguyen BuI <longro3000@gmail.com>
2020-10-23 13:41:08 +0300

    ad jwt guest auth guard

--- src/auth/jwt.guard.ts
+++ src/auth/jwt.guard.ts
@@ -7,11 +7,26 @@ import {
 import { AuthGuard } from '@nestjs/passport'
 import { Reflector } from '@nestjs/core'
 
-import { JWT_AUTH_USER, NO_AUTH } from './auth.const'
+import { JWT_AUTH_USER, NO_AUTH, JWT_AUTH_GUEST } from './auth.const'
 import { UserType } from '../auth/auth.type'
 
 @Injectable()
 export class UserJwtAuthGuard extends AuthGuard(JWT_AUTH_USER) {
+  constructor(private reflector: Reflector) {
+    super()
+  }
+
+  handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
+    const noAuth = this.reflector.get<string[]>(NO_AUTH, context.getHandler())
+    if (noAuth) {
+      return user
+    }
+    return super.handleRequest(err, user, info, context)
+  }
+}
+
+@Injectable()
+export class GuestJwtAuthGuard extends AuthGuard(JWT_AUTH_GUEST) {
   constructor(private reflector: Reflector) {
     super()
   }
